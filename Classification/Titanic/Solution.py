954eb9af8a6fc7718f226902f9813ed236c5dad0
AviaTorX <shinde91avinash@gmail.com>
2017-02-07 17:08:04 +0530

    Add files via upload

--- Classification/Titanic/Solution.py
+++ Classification/Titanic/Solution.py
@@ -1,68 +1,167 @@
 import pandas as pd
+from sklearn import svm
 import numpy as np
-from sklearn import tree
-from sklearn.ensemble import RandomForestClassifier
-train_path = "train.csv"
-test_path = "test.csv"
-train = pd.read_csv(train_path)
-test = pd.read_csv(test_path)
-#convert values into numeric Form
-train["Sex"][train["Sex"] == 'male'] = 0
-train["Sex"][train["Sex"] == 'female'] = 1
-train["Embarked"] = train["Embarked"].fillna('S')
+
+train = pd.read_csv("train.csv")
+test = pd.read_csv("test.csv")
+
+"""print("Train Info")
+print(train.info())
+print("=========================")
+print("Test info")
+print(test.info())
+print("=========================")
+
+train["Sex"][train["Sex"] == "male"] = 0
+train["Sex"][train["Sex"] == "female"] = 1
+train["Age"] = train["Age"].fillna(train["Age"].mean())
+train["Fare"] = train["Fare"].fillna(train["Fare"].mean())
+train["Embarked"].fillna("S")
 train["Embarked"][train["Embarked"] == "S"] = 0
 train["Embarked"][train["Embarked"] == "C"] = 1
 train["Embarked"][train["Embarked"] == "Q"] = 2
-train["Fare"] = train["Fare"].fillna(train["Fare"].median())
-train["Age"] = train["Age"].fillna(train["Age"].median())
+x = train[["Sex", "Age", "Fare", "Pclass"]]
+y = train["Survived"].values
 
-target = train["Survived"].values
+#print(train["Sex"].isnan())
+clf = svm.LinearSVC()
+clf.fit(x, y)
 
-"""feature_one = train[["Pclass", "Sex", "Age", "Fare"]].values
-my_tree_one = tree.DecisionTreeClassifier()
-my_tree_one = my_tree_one.fit(feature_one, target)"""
-#print(my_tree_one.feature_importances_)
-#print(my_tree_one.score(feature_one, target))
-   
-test["Sex"][test["Sex"] == 'male'] = 0
-test["Sex"][test["Sex"] == 'female'] = 1
-test["Embarked"] = test["Embarked"].fillna('S')
+test["Sex"][test["Sex"] == "male"] = 0
+test["Sex"][test["Sex"] == "female"] = 1
+test["Age"] = test["Age"].fillna(test["Age"].mean())
+test["Fare"] = test["Fare"].fillna(test["Fare"].mean())
+test["Embarked"].fillna("S")
 test["Embarked"][test["Embarked"] == "S"] = 0
 test["Embarked"][test["Embarked"] == "C"] = 1
 test["Embarked"][test["Embarked"] == "Q"] = 2
-test["Fare"] = test["Fare"].fillna(test["Fare"].median())
-test["Age"] = test["Age"].fillna(train["Age"].median())
-#test_feature = test[["Pclass", "Sex", "Age", "Fare"]]
-"""my_prediction = my_tree_one.predict(test_feature)
+test_feature = test[["Sex", "Age", "Fare", "Pclass"]]
+result = clf.predict(test_feature)
+
 PassengerId = np.array(test["PassengerId"]).astype(int)
-my_solution = pd.DataFrame(my_prediction, PassengerId, columns = ["Survived"])
-#print(my_solution)
-#print(my_solution.shape)
-my_solution.to_csv("result.csv", index_label = ["PassengerId"])"""
+solution = pd.DataFrame(result, PassengerId, columns = ["Survived"])
+#solution.to_csv("result.csv", index_label = ["PassengerId"])
 
-"""feature_two = train[["Pclass", "Sex", "Age", "Fare", "SibSp", "Parch", "Embarked"]]
-max_depth = 10
-min_samples_split = 5
-my_tree_two = tree.DecisionTreeClassifier(max_depth = 50, min_samples_split = 1, random_state = 1)
-my_tree_two = my_tree_two.fit(feature_two, target)
+#print("SVC Score = ",clf.score(x, y))"""
 
-print(my_tree_two.score(feature_two, target))"""
-"""train_two = train.copy()
-train_two["family_size"] = train_two["SibSp"] + train_two["Parch"] + 1
-feature_three = train_two[["Pclass", "Sex", "Age", "Fare", "SibSp", "Parch", "Embarked", "family_size"]]
-my_tree_three = tree.DecisionTreeClassifier()
-my_tree_three = my_tree_three.fit(feature_three, target)
-print(my_tree_three.score(feature_three, target))"""
+"""train["SibSp"].fillna(train["SibSp"].mean())
+train["Parch"].fillna(train["Parch"].mean())
 
-train["family_size"] = train["SibSp"] + train["Parch"] + 1
-features_forest = train[["Pclass", "Sex", "Age", "Fare", "SibSp", "Parch", "Embarked", "family_size"]]
-forest = RandomForestClassifier(max_depth = 10, min_samples_split = 2, n_estimators = 100, random_state = 1)
-my_forest = forest.fit(features_forest, target)
+x1 = train[["Sex","SibSp","Parch"  "Fare", "Age"]]
 
-print(my_forest.score(features_forest, target))
-test["family_size"] = test["SibSp"] + test["Parch"] + 1
-features_forest_test = test[["Pclass", "Sex", "Age", "Fare", "SibSp", "Parch", "Embarked", "family_size"]]
-solution = my_forest.predict(features_forest_test)
-PassengerId = np.array(test["PassengerId"]).astype(int)
-my_solution = pd.DataFrame(solution, PassengerId, columns = ["Survived"])
-my_solution.to_csv("result.csv", index_label = ["PassengerId"])
+clf1 = svm.SVC()
+clf1.fit(x1, y)
+
+print("SVC with Extra Features %f", clf1.score(x1, y))
+#print(result)"""
+
+def names(train, test):
+	for i in [train, test]:
+		i['Name_Len'] = i['Name'].apply(lambda x: len(x))
+		i['Name_Title'] = i['Name'].apply(lambda x: x.split(',')[1]).apply(lambda x: x.split()[0])
+		del i['Name']
+	return train, test
+
+def age_impute(train, test):
+    for i in [train, test]:
+        i['Age_Null_Flag'] = i['Age'].apply(lambda x: 1 if pd.isnull(x) else 0)
+        data = train.groupby(['Name_Title', 'Pclass'])['Age']
+        i['Age'] = data.transform(lambda x: x.fillna(x.mean()))
+    return train, test
+
+def fam_size(train, test):
+    for i in [train, test]:
+        i['Fam_Size'] = np.where((i['SibSp']+i['Parch']) == 0 , 'Solo',
+                           np.where((i['SibSp']+i['Parch']) <= 3,'Nuclear', 'Big'))
+        del i['SibSp']
+        del i['Parch']
+    return train, test
+
+def ticket_grouped(train, test):
+    for i in [train, test]:
+        i['Ticket_Lett'] = i['Ticket'].apply(lambda x: str(x)[0])
+        i['Ticket_Lett'] = i['Ticket_Lett'].apply(lambda x: str(x))
+        i['Ticket_Lett'] = np.where((i['Ticket_Lett']).isin(['1', '2', '3', 'S', 'P', 'C', 'A']), i['Ticket_Lett'],
+                                   np.where((i['Ticket_Lett']).isin(['W', '4', '7', '6', 'L', '5', '8']),
+                                            'Low_ticket', 'Other_ticket'))
+        i['Ticket_Len'] = i['Ticket'].apply(lambda x: len(x))
+        del i['Ticket']
+    return train, test
+
+def cabin(train, test):
+    for i in [train, test]:
+        i['Cabin_Letter'] = i['Cabin'].apply(lambda x: str(x)[0])
+        del i['Cabin']
+    return train, test
+
+def cabin_num(train, test):
+    for i in [train, test]:
+        i['Cabin_num1'] = i['Cabin'].apply(lambda x: str(x).split(' ')[-1][1:])
+        i['Cabin_num1'].replace('an', np.NaN, inplace = True)
+        i['Cabin_num1'] = i['Cabin_num1'].apply(lambda x: int(x) if not pd.isnull(x) and x != '' else np.NaN)
+        i['Cabin_num'] = pd.qcut(train['Cabin_num1'],3)
+    train = pd.concat((train, pd.get_dummies(train['Cabin_num'], prefix = 'Cabin_num')), axis = 1)
+    test = pd.concat((test, pd.get_dummies(test['Cabin_num'], prefix = 'Cabin_num')), axis = 1)
+    del train['Cabin_num']
+    del test['Cabin_num']
+    del train['Cabin_num1']
+    del test['Cabin_num1']
+    return train, test
+
+
+def embarked_impute(train, test):
+    for i in [train, test]:
+        i['Embarked'] = i['Embarked'].fillna('S')
+    return train, test
+
+def dummies(train, test, columns = ['Pclass', 'Sex', 'Embarked', 'Ticket_Lett', 'Cabin_Letter', 'Name_Title', 'Fam_Size']):
+    for column in columns:
+        train[column] = train[column].apply(lambda x: str(x))
+        test[column] = test[column].apply(lambda x: str(x))
+        good_cols = [column+'_'+i for i in train[column].unique() if i in test[column].unique()]
+        train = pd.concat((train, pd.get_dummies(train[column], prefix = column)[good_cols]), axis = 1)
+        test = pd.concat((test, pd.get_dummies(test[column], prefix = column)[good_cols]), axis = 1)
+        del train[column]
+        del test[column]
+    return train, test
+
+def drop(train, test, bye = ['PassengerId']):
+    for i in [train, test]:
+        for z in bye:
+            del i[z]
+    return train, test
+
+import os
+
+train, test = names(train, test)
+train, test = age_impute(train, test)
+train, test = cabin_num(train, test)
+train, test = cabin(train, test)
+train, test = embarked_impute(train, test)
+train, test = fam_size(train, test)
+test['Fare'].fillna(train['Fare'].mean(), inplace = True)
+train, test = ticket_grouped(train, test)
+train, test = dummies(train, test, columns = ['Pclass', 'Sex', 'Embarked', 'Ticket_Lett',
+                                                                     'Cabin_Letter', 'Name_Title', 'Fam_Size'])
+train, test = drop(train, test)
+
+from sklearn.ensemble import RandomForestClassifier
+
+"""train["Embarked"][train["Embarked"] == "S"] = 0
+train["Embarked"][train["Embarked"] == "C"] = 1
+train["Embarked"][train["Embarked"] == "Q"] = 2
+test["Embarked"][test["Embarked"] == "S"] = 0
+test["Embarked"][test["Embarked"] == "C"] = 1
+test["Embarked"][test["Embarked"] == "Q"] = 2"""
+rf = RandomForestClassifier(criterion='gini', n_estimators=700,min_samples_split=10,min_samples_leaf=1,max_features='auto',oob_score=True,random_state=1,n_jobs=-1)
+
+
+rf.fit(train.iloc[:, 1:], train.iloc[:, 0])
+#print("%.4f" % rf.oob_score_)
+
+predictions = rf.predict(test)
+predictions = pd.DataFrame(predictions, columns=['Survived'])
+test = pd.read_csv(os.path.join('test.csv'))
+predictions = pd.concat((test.iloc[:, 0], predictions), axis = 1)
+predictions.to_csv('y_test15.csv', sep=",", index = False)
+
